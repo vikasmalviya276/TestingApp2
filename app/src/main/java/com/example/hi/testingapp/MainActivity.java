f36357a453b7cdc5828e2d8f52752aa7b001feb4
vikasmalviya276 <vikas.malviya276@gmail.com>
2016-09-02 19:15:36 +0530

    Chaneg state

--- app/src/main/java/com/example/hi/testingapp/MainActivity.java
+++ app/src/main/java/com/example/hi/testingapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.hi.testingapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
     }
 }
