1a1b3451b1dfbe0059f9283f8893a6ba31fa3b45
vikasmalviya276 <vikas.malviya276@gmail.com>
2016-09-04 18:46:24 +0530

    Chaneg state

--- app/src/main/java/com/example/hi/testingapp/MainActivity.java
+++ app/src/main/java/com/example/hi/testingapp/MainActivity.java
@@ -1,17 +1,33 @@
 package com.example.hi.testingapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "mytag";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
-        Toast.makeText(this, "Message", Toast.LENGTH_SHORT).show();
     }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    public void click(View view) {
+        for (int i = 0; i < 10; i++) {
+            Log.i("TAG", "click: ITERATION" + i);
+        }
+
+    }
+
 }
+
